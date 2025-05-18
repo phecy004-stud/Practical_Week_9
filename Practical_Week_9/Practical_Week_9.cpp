e07243c08db9ef3bb592053947b82ceda8965911
nich-phe <98563980+nich-phe@users.noreply.github.com>
2025-05-18 19:12:15 +0930

    Completed Activity 2 Step 5 to 6 in the main() function and the results came out as expected

--- Practical_Week_9/Practical_Week_9.cpp
+++ Practical_Week_9/Practical_Week_9.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include "divide.h"
+#include "point.h"
 
 int main()
 {
@@ -10,6 +11,18 @@ int main()
     std::cout << divide<double>(5.5, 2.2) << std::endl; // return 2.5
     std::cout << divide<float>(10.10f, 5.5f) << std::endl; // return 1.83636
 	std::cout << divide<long>(100L, 5L) << std::endl; // return 20
+
+    // Step 5: Points with int
+    Point<int> pt1(2, 4);
+    Point<int> pt2(3, 5);
+    Point<int> pt3 = pt1 + pt2;
+    std::cout << "pt3 (int): (" << pt3.x() << ", " << pt3.y() << ")\n"; // (5, 9)
+
+    // Step 6: Points with double
+    Point<double> ptd1(2.6, 4.8);
+    Point<double> ptd2(3.7, 5.9);
+    Point<double> ptd3 = ptd1 + ptd2;
+    std::cout << "ptd3 (double): (" << ptd3.x() << ", " << ptd3.y() << ")\n";// (6.3, 10.7)
 
     return 0;
 }
